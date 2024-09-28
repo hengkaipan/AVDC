1f6b9d68e5385ea44b52c8af859d6d491d020f9c
Hengkai Pan <hp2190@nyu.edu>
2024-09-28 00:01:42 -0400

    rename ds; init train_ours

--- flowdiffusion/train_thor.py
+++ flowdiffusion/train_thor.py
@@ -1,7 +1,7 @@
 from goal_diffusion import GoalGaussianDiffusion, Trainer
 from unet import UnetThor as Unet
 from transformers import CLIPTextModel, CLIPTokenizer
-from datasets import SequentialNavDataset
+from datasets_avdc import SequentialNavDataset
 from torch.utils.data import Subset
 import argparse
 
@@ -11,15 +11,17 @@ def main(args):
     sample_per_seq = 8
     target_size = (64, 64)
 
-    if args.mode == 'inference':
-        train_set = valid_set = [None] # dummy
+    if args.mode == "inference":
+        train_set = valid_set = [None]  # dummy
     else:
         train_set = SequentialNavDataset(
-            sample_per_seq=sample_per_seq, 
-            path="../datasets/thor", 
+            sample_per_seq=sample_per_seq,
+            path="../datasets/thor",
             target_size=target_size,
         )
-        valid_inds = [i for i in range(0, len(train_set), len(train_set)//valid_n)][:valid_n]
+        valid_inds = [i for i in range(0, len(train_set), len(train_set) // valid_n)][
+            :valid_n
+        ]
         valid_set = Subset(train_set, valid_inds)
     unet = Unet()
 
@@ -30,41 +32,41 @@ def main(args):
     text_encoder.eval()
 
     diffusion = GoalGaussianDiffusion(
-        channels=3*(sample_per_seq-1),
+        channels=3 * (sample_per_seq - 1),
         model=unet,
         image_size=target_size,
         timesteps=100,
         sampling_timesteps=args.sample_steps,
-        loss_type='l2',
-        objective='pred_v',
-        beta_schedule = 'cosine',
-        min_snr_loss_weight = True,
+        loss_type="l2",
+        objective="pred_v",
+        beta_schedule="cosine",
+        min_snr_loss_weight=True,
     )
 
     trainer = Trainer(
         diffusion_model=diffusion,
-        tokenizer=tokenizer, 
+        tokenizer=tokenizer,
         text_encoder=text_encoder,
         train_set=train_set,
         valid_set=valid_set,
         train_lr=1e-4,
-        train_num_steps =80000,
-        save_and_sample_every =5000,
-        ema_update_every = 10,
-        ema_decay = 0.999,
-        train_batch_size =32,
-        valid_batch_size =32,
-        gradient_accumulate_every = 1,
-        num_samples=valid_n, 
-        results_folder ='../results/thor',
-        fp16 =True,
+        train_num_steps=80000,
+        save_and_sample_every=5000,
+        ema_update_every=10,
+        ema_decay=0.999,
+        train_batch_size=32,
+        valid_batch_size=32,
+        gradient_accumulate_every=1,
+        num_samples=valid_n,
+        results_folder="../results/thor",
+        fp16=True,
         amp=True,
     )
 
     if args.checkpoint_num is not None:
         trainer.load(args.checkpoint_num)
-    
-    if args.mode == 'train':
+
+    if args.mode == "train":
         trainer.train()
     else:
         from PIL import Image
@@ -72,33 +74,51 @@ def main(args):
         import imageio
         import torch
         from os.path import splitext
+
         text = args.text
         image = Image.open(args.inference_path)
         batch_size = 1
-        transform = transforms.Compose([
-            transforms.Resize(target_size),
-            transforms.ToTensor(),
-        ])
+        transform = transforms.Compose(
+            [
+                transforms.Resize(target_size),
+                transforms.ToTensor(),
+            ]
+        )
         image = transform(image)
         output = trainer.sample(image.unsqueeze(0), [text], batch_size).cpu()
         output = output[0].reshape(-1, 3, *target_size)
         output = torch.cat([image.unsqueeze(0), output], dim=0)
         root, ext = splitext(args.inference_path)
-        output_gif = root + '_out.gif'
-        output = (output.cpu().numpy().transpose(0, 2, 3, 1).clip(0, 1) * 255).astype('uint8')
+        output_gif = root + "_out.gif"
+        output = (output.cpu().numpy().transpose(0, 2, 3, 1).clip(0, 1) * 255).astype(
+            "uint8"
+        )
         imageio.mimsave(output_gif, output, duration=200, loop=1000)
-        print(f'Generated {output_gif}')
+        print(f"Generated {output_gif}")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--mode', type=str, default='train', choices=['train', 'inference']) # set to 'inference' to generate samples
-    parser.add_argument('-c', '--checkpoint_num', type=int, default=None) # set to checkpoint number to resume training or generate samples
-    parser.add_argument('-p', '--inference_path', type=str, default=None) # set to path to generate samples
-    parser.add_argument('-t', '--text', type=str, default=None) # set to text to generate samples
-    parser.add_argumant('-n', '--sample_steps', type=int, default=100) # set to number of steps to sample
-    parser.add_argument('-g', '--guidance_weight', type=int, default=0) # set to positive to use guidance
+    parser.add_argument(
+        "-m", "--mode", type=str, default="train", choices=["train", "inference"]
+    )  # set to 'inference' to generate samples
+    parser.add_argument(
+        "-c", "--checkpoint_num", type=int, default=None
+    )  # set to checkpoint number to resume training or generate samples
+    parser.add_argument(
+        "-p", "--inference_path", type=str, default=None
+    )  # set to path to generate samples
+    parser.add_argument(
+        "-t", "--text", type=str, default=None
+    )  # set to text to generate samples
+    parser.add_argumant(
+        "-n", "--sample_steps", type=int, default=100
+    )  # set to number of steps to sample
+    parser.add_argument(
+        "-g", "--guidance_weight", type=int, default=0
+    )  # set to positive to use guidance
     args = parser.parse_args()
-    if args.mode == 'inference':
+    if args.mode == "inference":
         assert args.checkpoint_num is not None
         assert args.inference_path is not None
         assert args.text is not None
